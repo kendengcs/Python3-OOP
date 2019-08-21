b89e94a2d1d4c05bfb855d460e26662511b2675c
Ken Deng <kendeng.cs@gmail.com>
2019-08-21 15:12:37 -0400

    change number to 99

--- ch5/guess_number.py
+++ ch5/guess_number.py
@@ -1,14 +1,12 @@
 import random
 
 b = random.randint(1,99)
-#b = 20
 while True:
-    a = int(input("Please input a number from 1-99: "))
-
-    if (a==b):
-        print("You made it!")
+    a = int(input("Please input a number from 1 to 99"))
+    if (a == b):
+        print("Great, you made it!")
         break
-    elif a > b:
-        print("Too large, try again")
+    elif a > b :
+        print("It's too large, try again~")
     else:
-        print("Too small, try again")
+        print("It's too small, try again~")

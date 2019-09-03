5709cc2ccb1a40d03cbad08791dcd3aa27064a11
Ken Deng <kendeng.cs@gmail.com>
2019-09-02 21:54:04 -0400

    Update inheritance.py

--- ch7/inheritance.py
+++ ch7/inheritance.py
@@ -4,7 +4,34 @@ class Animal:
         print('Animal is eating')
 
 class Bird(Animal):
-    pass
+    def sing(self):
+        print('Bird is singing...')
 
+    def eat(self):
+        print('Bird is eating')
+
+class Dog(Animal):
+    def eat(self):
+        print('Dog is eating')
+
+class Cat(Animal):
+    # def eat(self):
+    #     print('Cat is eating')
+    pass
+a = Animal()
+# a.eat()
+#
 b = Bird()
-b.eat()
+# b.eat()
+# b.sing()
+#
+d = Dog()
+# d.eat()
+
+c = Cat()
+
+def demo_eat(a):
+    a.eat()
+
+for item in [a, b, c, d]:
+    demo_eat(item)

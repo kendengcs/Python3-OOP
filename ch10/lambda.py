0ad1d8c90e42797dd403b5341eb748f381b4b180
Ken Deng <kendeng.cs@gmail.com>
2019-09-23 00:17:27 -0400

    ch10
    lambda

---
+++ ch10/lambda.py
@@ -0,0 +1,22 @@
+#lambda 匿名函数
+
+def test(x, y):
+    return x + 2 * y
+
+#f = lambda x, y: x + 2* y
+
+def demo(x, y, f):
+    return f(x,y)
+
+#print(test(1,2))
+
+#print(f(1,2))
+
+print(demo (1,2, lambda x, y: x + 2* y))
+
+def add_n(n):
+    return lambda x: n + x
+f = add_n(40)
+
+print(f(1))
+print(f(-10))
---
+++ ch9/file_write.py
@@ -0,0 +1,5 @@
+f = open("test1.txt", mode='x') #'x': create a new file and open it for writing
+
+f.write('line 1')
+
+f.close()#no close, no save written
